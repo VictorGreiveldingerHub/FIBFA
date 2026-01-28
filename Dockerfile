3d78dcd5008f28bccc0ff3aa92c9c2fd9096e00b
Victor Greiveldinger <victorgreiveldinger@gmail.com>
2026-01-28 15:37:23 +0100

    rectification probleme de port avec axios + nodemon (dev) pour docker

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /usr/src/app
 
 # Copy package files and install deps
 COPY package*.json ./
-RUN npm install
+RUN npm install --include=dev
 
 # Copy project files
 COPY . .
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.PORT || 54520}`,
+  baseURL: `http://localhost:54520}`, // Ici, je mets volontairement 54520 en dur,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
